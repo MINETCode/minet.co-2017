de19cffea8ee6a1ec93b91d343d99f2bf1631ee8
Nishant Gadihoke <ngadihoke8@gmail.com>
2017-08-01 21:37:26 +0530

    Bug fixes + nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
 
 //Render home page
 router.get('/', (req, res, next) => {
@@ -29,6 +31,33 @@ router.get('/schedule', (req, res, next) => {
 //Render request page
 router.get('/request', (req, res, next) => {
   return res.render('request');
+});
+
+//Render request page
+router.post('/request', (req, res, next) => {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'minet@themis.in', // Your email id
+      pass: '1JagdishKumar01*' // Your password
+    }
+  });
+  var text = req.body.insName + ' has requested an invite for MINET X 2017. \n\n Name of institution:' + req.body.insName + '\n Contact email: ' + req.body.email + '\n Point of contact: ' + req.body.pName + '\n Website/Relevant links: ' + req.body.links;
+  var mailOptions = {
+    from: 'minet@themis.in', // sender address
+    to: 'minet+requests@themis.in', // list of receivers
+    subject: 'Invitation Request for MINET X', // Subject line
+    text: text //, // plaintext body
+    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error){
+        console.log(error);
+        res.render('/error');
+    } else{
+        res.render('/success');
+    };
+  });
 });
 
 //Render rsvp page
